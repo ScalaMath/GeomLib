fb68738c465b896c3b97210afb3d1761d525addf
Nico <nico.hex6@gmail.com>
2024-05-01 12:27:45 +0200

    Plane transformation

--- src/main/scala/io/github/scalamath/geomlib/Plane.scala
+++ src/main/scala/io/github/scalamath/geomlib/Plane.scala
@@ -1,12 +1,12 @@
 package io.github.scalamath.geomlib
 
 import io.github.scalamath.FloatEqualsApprox
-import io.github.scalamath.vecmatlib.Vec3f
+import io.github.scalamath.vecmatlib.{Mat3f, Mat3x4f, Mat4f, Vec3f}
 
 /**
- * A plane in Hessian normal form.
+ * A plane defined by a normal vector and its distance from the origin.
  *
- * A plane is defined by the equation `ax + by + cz + d = 0`, where the vector `(a, b, c)` is the normal of the plane and `d` is the distance from the origin to the plane.
+ * The equation of a plane is `ax + by + cz = d`, where the vector `(a, b, c)` is the normal of the plane and `d` is the distance from the origin to the plane.
  *
  * @constructor Constructs a plane with the given normal and distance from the origin.
  * @param normal The normal of the plane. Not required to be a unit vector. Should not be the zero vector.
@@ -22,7 +22,7 @@ case class Plane(normal: Vec3f, d: Float) {
   def this(normal: Vec3f) = this(normal, 0.0f)
 
   /**
-   * Constructs a plane from the given scalar equation.
+   * Constructs a plane with the equation `ax + by + cz = d`.
    *
    * @param a The x component of the plane's normal.
    * @param b The y component of the plane's normal.
@@ -32,7 +32,7 @@ case class Plane(normal: Vec3f, d: Float) {
   def this(a: Float, b: Float, c: Float, d: Float) = this(Vec3f(a, b, c), d)
 
   /**
-   * Constructs a plane passing through the origin with the given scalar equation.
+   * Constructs a plane passing through the origin with the equation `ax + by + cz = 0`.
    *
    * @param x The x component of the plane's normal.
    * @param y The y component of the plane's normal.
@@ -110,6 +110,26 @@ case class Plane(normal: Vec3f, d: Float) {
   def distanceTo(point: Vec3f): Float = this.distanceTo(point.x, point.y, point.z)
 
   /**
+   * Flips the direction of this plane's normal and its distance value and returns the result.
+   * The resulting plane is congruent to the original one, but faces the opposite direction.
+   *
+   * This method can be used in place of the unary `-` operator for better interoperability with Java.
+   *
+   * @return A plane that is congruent to the original one, but faces the opposite direction.
+   */
+  def flip: Plane = Plane(-this.normal, -this.d)
+
+  /**
+   * Flips the direction of this plane's normal and its distance value and returns the result.
+   * The resulting plane is congruent to the original one, but faces the opposite direction.
+   *
+   * This operator is equivalent to the [[flip]] method.
+   *
+   * @return A plane that is congruent to the original one, but faces the opposite direction.
+   */
+  def unary_- : Plane = this.flip
+
+  /**
    * Checks if the point at the given coordinates belongs to this plane.
    *
    * @param x The x coordinate of the point.
@@ -174,7 +194,7 @@ case class Plane(normal: Vec3f, d: Float) {
    * @return True if this plane is congruent to the given one, otherwise false.
    * @see [[isParallelTo]]
    */
-  def isCongruentTo(plane: Plane): Boolean = this.isParallelTo(plane) && (this.d ~= plane.d)
+  def isCongruentTo(plane: Plane): Boolean = this.isParallelTo(plane) && ((this.d / this.normal.length).abs ~= (plane.d / plane.normal.length).abs)
 
   /**
    * Checks if this plane intersects the given one.
@@ -182,7 +202,7 @@ case class Plane(normal: Vec3f, d: Float) {
    * @param plane The second plane.
    * @return True if this plane intersects the given one, otherwise false.
    */
-  def intersects(plane: Plane): Boolean = !this.isParallelTo(plane) && !(this.d ~= plane.d)
+  def intersects(plane: Plane): Boolean = !this.isParallelTo(plane) || ((this.d / this.normal.length) ~= (plane.d / plane.normal.length))
 
   /**
    * Checks if this plane and the given ones intersect in a single point.
@@ -193,14 +213,14 @@ case class Plane(normal: Vec3f, d: Float) {
    * @param plane2 The third plane.
    * @return True if the three planes intersect in a single point, otherwise false.
    */
-  def intersects(plane1: Plane, plane2: Plane): Boolean = !(this.normal.cross(plane1.normal).dot(plane2.normal) ~= 0.0f)
+  def intersectsInPoint(plane1: Plane, plane2: Plane): Boolean = !(this.normal.cross(plane1.normal).dot(plane2.normal) ~= 0.0f)
 
   /**
    * Returns the point of intersection between this plane and the given ones.
    *
    * If all the three planes are parallel, all the components of the resulting vector will be `NaN`.
    * If the intersection between the three planes is a line, the components of the resulting vector will be either `NaN` or `Infinity`.
-   * The validity of the result can be checked using [[intersects]].
+   * The validity of the result can be checked using [[intersectsInPoint]].
    *
    * @param plane1 The second plane.
    * @param plane2 The third plane.
@@ -208,28 +228,240 @@ case class Plane(normal: Vec3f, d: Float) {
    */
   def intersection(plane1: Plane, plane2: Plane): Vec3f = (plane1.normal.cross(plane2.normal) * this.d + plane2.normal.cross(this.normal) * plane1.d + this.normal.cross(plane1.normal) * plane2.d) / this.normal.cross(plane1.normal).dot(plane2.normal)
 
-  def project(x: Float, y: Float, z: Float): Vec3f = Vec3f(x - this.a, y - this.b, z - this.c) * this.distanceTo(x, y, z)
-
   def project(point: Vec3f): Vec3f = point - this.normal * this.distanceTo(point)
 
+  def project(x: Float, y: Float, z: Float): Vec3f = this.project(Vec3f(x, y, z))
+
+  /**
+   * Checks if the point at the given coordinates is "above" this plane.
+   * Which side of the plane is the "above" side is determined by the direction of the plane's normal.
+   *
+   * @param x The x coordinate of the point.
+   * @param y The y coordinate of the point.
+   * @param z The z coordinate of the point.
+   * @return True if the point at the given coordinates is "above" this plane, otherwise false.
+   */
   def isPointOver(x: Float, y: Float, z: Float): Boolean = this.normal.dot(x, y, z) > this.d
 
+  /**
+   * Checks if the given point is "above" this plane.
+   * Which side of the plane is the "above" side is determined by the direction of the plane's normal.
+   *
+   * @param point The point.
+   * @return True if the given point is "above" this plane, otherwise false.
+   */
   def isPointOver(point: Vec3f): Boolean = this.normal.dot(point) > this.d
 
-  def normalized: Plane = Plane(this.normal.normalized, this.d)
+  /**
+   * Transforms this plane by the given matrix under the assumption that it is a valid transformation matrix and returns the result.
+   *
+   * This method can be used in place of the `*` operator for better interoperability with Java.
+   *
+   * @param m A 3x3 transformation matrix.
+   * @return The resulting plane.
+   */
+  def transform(m: Mat3f): Plane = Plane(m.inverse.transposed * this.normal, m * (this.normal * this.d))
 
+  /**
+   * Transforms this plane by the given matrix under the assumption that it is a valid transformation matrix and returns the result.
+   *
+   * This method can be used in place of the `*` operator for better interoperability with Java.
+   *
+   * @param m A 3x4 transformation matrix.
+   * @return The resulting plane.
+   */
+  def transform(m: Mat3x4f): Plane = Plane(Mat3f.fromColumns(m.col0, m.col1, m.col2).inverse.transposed * this.normal, m * (this.normal * this.d, 1.0f))
+  // TODO: Add a better way to extract a 3x3 submatrix from a 3x4 matrix
+
+  /**
+   * Transforms this plane by the given matrix under the assumption that it is a valid transformation matrix and returns the result.
+   *
+   * This method can be used in place of the `*` operator for better interoperability with Java.
+   *
+   * @param m A 4x4 transformation matrix.
+   * @return The resulting plane.
+   */
+  def transform(m: Mat4f): Plane = Plane((m.inverse.transposed * (this.normal, 1.0f)).xyz, (m * (this.normal * this.d, 1.0f)).xyz)
+
+  /**
+   * Inversely transforms this plane by the given matrix under the assumption that it is a valid transformation matrix and returns the result.
+   *
+   * This method can be used in place of the `*` operator for better interoperability with Java.
+   *
+   * `plane.inverseTransform(matrix)` is equivalent to `plane.transform(matrix.inverse)`.
+   *
+   * @param m A 3x3 transformation matrix.
+   * @return The resulting plane.
+   */
+  def inverseTransform(m: Mat3f): Plane = Plane(m.transposed * this.normal, m.inverse * (this.normal * this.d))
+
+  /**
+   * Inversely transforms this plane by the given matrix under the assumption that it is a valid transformation matrix and returns the result.
+   *
+   * This method can be used in place of the `*` operator for better interoperability with Java.
+   *
+   * `plane.inverseTransform(matrix)` is equivalent to `plane.transform(matrix.inverse)`.
+   *
+   * @param m A 4x4 transformation matrix.
+   * @return The resulting plane.
+   */
+  def inverseTransform(m: Mat4f): Plane = Plane((m.transposed * (this.normal, 1.0f)).xyz, (m.inverse * (this.normal * this.d, 1.0f)).xyz)
+
+  /**
+   * Inversely transforms this plane by the given matrix under the assumption that it is a valid transformation matrix and returns the result.
+   *
+   * `plane * matrix` is equivalent to `matrix.inverse * plane`.
+   *
+   * @param m A 3x3 transformation matrix.
+   * @return The resulting plane.
+   */
+  def *(m: Mat3f): Plane = this.inverseTransform(m)
+
+  /**
+   * Inversely transforms this plane by the given matrix under the assumption that it is a valid transformation matrix and returns the result.
+   *
+   * `plane * matrix` is equivalent to `matrix.inverse * plane`.
+   *
+   * @param m A 4x4 transformation matrix.
+   * @return The resulting plane.
+   */
+  def *(m: Mat4f): Plane = this.inverseTransform(m)
+
+  /**
+   * Checks if this plane is approximately equal to the given one using an internal epsilon.
+   *
+   * This method checks if the normals and the distances `d` are approximately equal, therefore it may still return false for congruent planes with a different equation.
+   * Use [[isCongruentTo]] to check if two planes are congruent.
+   *
+   * @param plane The second plane.
+   * @return True if this plane is approximately equal to the given one, otherwise false.
+   */
   def ~=(plane: Plane): Boolean = (this.normal ~= plane.normal) && (this.d ~= plane.d)
 
+  /**
+   * Checks if this plane is approximately equal to the given one using an internal epsilon.
+   *
+   * This method can be used in place of the `~=` operator for better interoperability with Java.
+   *
+   * This method checks if the normals and the distances `d` are approximately equal, therefore it may still return false for congruent planes with a different equation.
+   * Use [[isCongruentTo]] to check if two planes are congruent.
+   *
+   * @param plane The second plane.
+   * @return True if this plane is approximately equal to the given one, otherwise false.
+   */
   def equalsApprox(plane: Plane): Boolean = this ~= plane
+
+  override def toString: String = f"Plane(${this.a}, ${this.b}, ${this.c}, ${this.d})"
 }
 
+/**
+ * Factory methods and implicits for planes.
+ */
 object Plane {
 
+  /**
+   * Constructs a plane passing through the origin with the given normal.
+   *
+   * This method allows to construct a plane without using the `new` keyword in Scala.
+   *
+   * @param normal The normal of the plane. Not required to be a unit vector. Should not be the zero vector.
+   * @return The newly instantiated plane.
+   */
   def apply(normal: Vec3f): Plane = new Plane(normal)
 
+  /**
+   * Constructs a plane with the equation `ax + by + cz = d`.
+   *
+   * This method allows to construct a plane without using the `new` keyword in Scala.
+   *
+   * @param a The x component of the plane's normal.
+   * @param b The y component of the plane's normal.
+   * @param c The z component of the plane's normal.
+   * @param d The distance from the origin to the plane.
+   * @return The newly instantiated plane.
+   */
   def apply(a: Float, b: Float, c: Float, d: Float): Plane = new Plane(a, b, c, d)
 
+  /**
+   * Constructs a plane passing through the origin with the equation `ax + by + cz = 0`.
+   *
+   * This method allows to construct a plane without using the `new` keyword in Scala.
+   *
+   * @param x The x component of the plane's normal.
+   * @param y The y component of the plane's normal.
+   * @param z The z component of the plane's normal.
+   * @return The newly instantiated plane.
+   */
+  def apply(x: Float, y: Float, z: Float): Plane = new Plane(x, y, z)
+
+  /**
+   * Constructs a plane passing through the given point with the given normal.
+   *
+   * This method allows to construct a plane without using the `new` keyword in Scala.
+   *
+   * @param normal The normal of the plane. Not required to be a unit vector. Should not be the zero vector.
+   * @param point A point the plane is passing through.
+   * @return The newly instantiated plane.
+   */
   def apply(normal: Vec3f, point: Vec3f): Plane = new Plane(normal, point)
 
+  /**
+   * Constructs a plane passing through the three given points.
+   *
+   * This method allows to construct a plane without using the `new` keyword in Scala.
+   *
+   * @param p1 The first point.
+   * @param p2 The second point.
+   * @param p3 The third point.
+   * @return The newly instantiated plane.
+   */
   def apply(p1: Vec3f, p2: Vec3f, p3: Vec3f): Plane = new Plane(p1, p2, p3)
+
+  /**
+   * Implicit class that extends [[Mat3f]] to add the `*` operator for planes.
+   *
+   * @param self This matrix.
+   */
+  implicit class Transform3x3(val self: Mat3f) extends AnyVal {
+
+    /**
+     * Transforms the given plane by the this matrix under the assumption that it is a valid transformation matrix and returns the result.
+     *
+     * @param plane The plane to transform.
+     * @return The resulting plane.
+     */
+    def *(plane: Plane): Plane = plane.transform(self)
+  }
+
+  /**
+   * Implicit class that extends [[Mat3x4f]] to add the `*` operator for planes.
+   *
+   * @param self This matrix.
+   */
+  implicit class Transform3x4(val self: Mat3x4f) extends AnyVal {
+
+    /**
+     * Transforms the given plane by the this matrix under the assumption that it is a valid transformation matrix and returns the result.
+     *
+     * @param plane The plane to transform.
+     * @return The resulting plane.
+     */
+    def *(plane: Plane): Plane = plane.transform(self)
+  }
+
+  /**
+   * Implicit class that extends [[Mat4f]] to add the `*` operator for planes.
+   *
+   * @param self This matrix.
+   */
+  implicit class Transform4x4(val self: Mat4f) extends AnyVal {
+
+    /**
+     * Transforms the given plane by the this matrix under the assumption that it is a valid transformation matrix and returns the result.
+     *
+     * @param plane The plane to transform.
+     * @return The resulting plane.
+     */
+    def *(plane: Plane): Plane = plane.transform(self)
+  }
 }
--- src/test/scala/io/github/scalamath/geomlib/PlaneSuite.scala
+++ src/test/scala/io/github/scalamath/geomlib/PlaneSuite.scala
@@ -1,13 +1,177 @@
 package io.github.scalamath.geomlib
 
-import io.github.scalamath.vecmatlib.Vec3f
+import io.github.scalamath.vecmatlib.{Mat3f, Mat3x4f, Mat4f, Vec3f}
 import org.scalatest.funsuite.AnyFunSuite
 
 class PlaneSuite extends AnyFunSuite {
 
-  test("Temp") {
-    val plane0 = Plane(0.0f, 2.0f, 0.0f, 0.0f)
-    val point = Vec3f(0.0f, -1.0f, 0.0f)
-    println(plane0.distanceTo(point))
+  test("Plane with a point and a normal") {
+    val normal = Vec3f(1.0f, 1.0f, 1.0f)
+    val point = Vec3f(0.5f, 0.5f, 1.0f)
+    val plane = Plane(normal, point)
+    val res = Plane(1.0f, 1.0f, 1.0f, 2.0f)
+    assert(plane == res)
+  }
+
+  test("Plane through three points") {
+    val p1 = Vec3f(-1.0f, -2.0f, 0.0f)
+    val p2 = Vec3f(-2.0f, 1.0f, 2.0f)
+    val p3 = Vec3f(1.5f, 1.5f, 1.0f)
+    val plane = Plane(p1, p2, p3)
+    val res = Plane(-4.0f, 6.0f, -11.0f, -8.0f)
+    assert(plane == res)
+  }
+
+  test("Get plane equation coefficients") {
+    val plane = Plane(1.0f, 2.0f, 3.0f, 4.0f)
+    assert(plane.a == 1.0f)
+    assert(plane.b == 2.0f)
+    assert(plane.c == 3.0f)
+    assert(plane.d == 4.0f)
+  }
+
+  ignore("Distance from point to plane") {
+    // TODO: Distance from point to plane
+  }
+
+  test("Flip plane") {
+    val plane = Plane(1.0f, 0.0f, 1.0f, -2.0f)
+    val res = Plane(-1.0f, 0.0f, -1.0f, 2.0f)
+    assert(-plane == res)
+  }
+
+  test("Check if plane contains points") {
+    val p1 = Vec3f(-1.0f, -2.0f, 0.0f)
+    val p2 = Vec3f(-2.0f, 1.0f, 2.0f)
+    val p3 = Vec3f(1.5f, 1.5f, 1.0f)
+    val plane = Plane(-4.0f, 6.0f, -11.0f, -8.0f)
+    assert(plane.containsPoint(p1))
+    assert(plane.containsPoint(p2))
+    assert(plane.containsPoint(p3))
+    assert(!plane.containsPoint(3.5f, -1.0f, 2.9f))
+  }
+
+  ignore("Check if plane intersects line") {
+    // TODO: Line plane intersection
+  }
+
+  ignore("Get line-plane intersection") {
+    // TODO: Line plane intersection
+  }
+
+  test("Check if two planes are parallel") {
+    val plane1 = Plane(0.0f, 1.0f, 0.0f)
+    val plane2 = Plane(0.0f, 2.0f, 0.0f, 5.0f)
+    assert(plane1.isParallelTo(plane2))
+    val plane3 = Plane(1.0f, 0.0f, 1.0f, 2.0f)
+    assert(!plane1.isParallelTo(plane3))
+    assert(plane1.isParallelTo(plane1))
+    val plane4 = Plane(2.0f, 0.0f, 2.0f, 4.0f)
+    assert(plane3.isParallelTo(plane4))
+  }
+
+  test("Check if two planes are congruent") {
+    val plane1 = Plane(0.0f, 1.0f, 0.0f, 3.0f)
+    val plane2 = Plane(0.0f, 1.0f, 0.0f, 3.0f)
+    val plane3 = Plane(0.0f, 2.0f, 0.0f, 6.0f)
+    assert(plane1.isCongruentTo(plane2))
+    assert(plane1.isCongruentTo(plane3))
+    val plane4 = Plane(0.0f, 2.0f, 0.0f, 5.0f)
+    assert(!plane1.isCongruentTo(plane4))
+    val plane5 = Plane(2.0f, 0.0f, 2.0f, 4.0f)
+    assert(!plane1.isCongruentTo(plane5))
+    val plane6 = Plane(0.2f, 0.0f, 0.2f, 0.4f)
+    assert(plane5.isCongruentTo(plane6))
+  }
+
+  test("Check if two planes intersect") {
+    val plane1 = Plane(0.0f, 1.0f, 0.0f, 3.0f)
+    val plane2 = Plane(1.0f, 0.0f, 1.0f, 2.0f)
+    assert(plane1.intersects(plane2))
+    val plane3 = Plane(0.0f, 1.0f, 0.0f, 5.0f)
+    assert(!plane1.intersects(plane3))
+    val plane4 = Plane(3.0f, 0.0f, 3.0f, 6.0f)
+    assert(plane2.intersects(plane4))
+  }
+
+  test("Check if three planes intersect in a single point") {
+    val plane1 = Plane(1.0f, 2.0f, 1.0f, 1.0f)
+    val plane2 = Plane(2.0f, 0.0f, 1.0f, 1.0f)
+    val plane3 = Plane(1.0f, 1.0f, 2.0f, 1.0f)
+    assert(plane1.intersectsInPoint(plane2, plane3))
+    val plane4 = Plane(0.0f, 1.0f, 0.0f, 3.0f)
+    val plane5 = Plane(0.0f, 1.0f, 0.0f, 7.0f)
+    val plane6 = Plane(1.0f, 0.0f, 0.0f, 2.0f)
+    assert(!plane4.intersectsInPoint(plane5, plane6))
+  }
+
+  test("Get the point of intersection between three planes") {
+    val plane1 = Plane(1.0f, 2.0f, 1.0f, 1.0f)
+    val plane2 = Plane(2.0f, 1.0f, 1.0f, 1.0f)
+    val plane3 = Plane(1.0f, 1.0f, 2.0f, 1.0f)
+    val res = Vec3f(0.25f, 0.25f, 0.25f)
+    assert(plane1.intersection(plane2, plane3) == res)
+  }
+
+  ignore("Project point onto plane") {
+    // TODO: Project point onto plane
+  }
+
+  test("Check if point is above plane") {
+    val plane1 = Plane(0.0f, 1.0f, 0.0f, 2.0f)
+    val p1 = Vec3f(1.0f, 4.0f, 1.0f)
+    val p2 = Vec3f(2.0f, -3.0f, 3.0f)
+    assert(plane1.isPointOver(p1))
+    assert(!plane1.isPointOver(p2))
+    val plane2 = Plane(0.0f, -1.0f, 0.0f, -2.0f)
+    assert(!plane2.isPointOver(p1))
+    assert(plane2.isPointOver(p2))
+    val plane3 = Plane(1.0f, 0.0f, 1.0f)
+    assert(plane3.isPointOver(1.0f, 0.0f, 1.0f))
+    assert(!plane3.isPointOver(-1.0f, 0.0f, -1.0f))
+  }
+
+  test("Rotate a plane with a 3x3 matrix") {
+    val transform = Mat3f.rotation(math.Pi / 2.0, 0.0, math.Pi / 4.0)
+    val plane = Plane(1.0f, 1.0f, 0.0f, 1.0f)
+    val res = Plane(0.0f, 0.0f, 1.0f, math.sqrt(2.0).toFloat)
+    assert((transform * plane).isCongruentTo(res))
+  }
+
+  test("Transform a plane with a 3x4 matrix") {
+    val transform = Mat3x4f.translation(0.0f, 0.0f, 1.0f) * Mat4f.rotation(math.Pi / 2.0, 0.0, math.Pi / 4.0)
+    val plane = Plane(1.0f, 1.0f, 0.0f, 1.0f)
+    val res = Plane(0.0f, 0.0f, 1.0f, math.sqrt(2.0).toFloat + 1.0f)
+    assert((transform * plane).isCongruentTo(res))
+  }
+
+  test("Transform a plane with a 4x4 matrix") {
+    val transform = Mat4f.translation(0.0f, 0.0f, 1.0f) * Mat4f.rotation(math.Pi / 2.0, 0.0, math.Pi / 4.0)
+    val plane = Plane(1.0f, 1.0f, 0.0f, 1.0f)
+    val res = Plane(0.0f, 0.0f, 1.0f, math.sqrt(2.0).toFloat + 1.0f)
+    assert((transform * plane).isCongruentTo(res))
+  }
+
+  test("Inverse transform a plane with a 3x3 matrix") {
+    val transform = Mat3f.rotation(math.Pi / 2.0, 0.0, math.Pi / 4.0)
+    val plane = Plane(1.0f, 1.0f, 0.0f, 1.0f)
+    val res = Plane(0.5f, -0.5f, -math.sqrt(2.0).toFloat / 2.0f, math.sqrt(2.0).toFloat)
+    assert((plane * transform).isCongruentTo(res))
+  }
+
+  test("Inverse transform a plane with a 4x4 matrix") {
+    val transform = Mat4f.translation(0.0f, 1.0f, 0.0f) * Mat4f.rotation(math.Pi / 2.0, 0.0, math.Pi / 4.0)
+    val plane = Plane(1.0f, 1.0f, 0.0f, 1.0f)
+    val res = Plane(0.5f, -0.5f, -math.sqrt(2.0).toFloat / 2.0f, math.sqrt(2.0).toFloat / 2.0f)
+    assert((plane * transform).isCongruentTo(res))
+  }
+
+  test("Check if two planes are approximately equal") {
+    val plane1 = Plane(1.0f, 2.0f, 3.0f, 4.0f)
+    val plane2 = Plane(0.9999999f, 1.9999999f, 3.0000001f, 4.0000001f)
+    val plane3 = -plane1
+    assert(plane1 ~= plane2)
+    assert(plane3.isCongruentTo(plane1))
+    assert(!(plane3 ~= plane1))
   }
 }
